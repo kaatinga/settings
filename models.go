31d399506a1b300a8040f4f5d75298f7129c303c
Kaatinga <painter@3lines.club>
2020-10-08 17:28:30 +0300

    new version of the settings loader started to be implemented (unfinished)

--- models.go
+++ models.go
@@ -1,1 +1,29 @@
 package env_loader
+
+import "errors"
+
+type Settings struct {
+	strings map[string]string
+	fixed   bool
+}
+
+func (s Settings) GetString(setting string) (value string, err error) {
+	// TBI
+	return
+}
+
+func (s Settings) GetByte(setting string) (value byte, err error) {
+	// TBI
+	return
+}
+
+// SetString sets an setting that must be in the environment variables
+func (s Settings) SetString(value string) error {
+	switch s.fixed {
+	case true:
+		return errors.New("impossible to add a setting as settings are read already")
+	case false:
+		// TBI
+	}
+	return nil
+}
