2aaa4d3a08422673849253f7f92704ed2b1814af
Kaatinga <painter@3lines.club>
2021-05-02 12:12:39 +0300

    constants added

--- consts.go
+++ consts.go
@@ -1,1 +1,15 @@
 package env_loader
+
+const (
+	// env — env tag name
+	env = "env"
+
+	// duration — type time.Duration
+	duration = "time.Duration"
+
+	// logLevel — type logrus.Level
+	logLevel = "logrus.Level"
+
+	// syslogPriority — type syslog.Priority
+	syslogPriority = "syslog.Priority"
+)
