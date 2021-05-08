73737a7700a1a94d6353141f88b33f8af8bdb3ab
Michael Gunkoff <painter@3lines.club>
2021-05-08 16:52:47 +0300

    added nolint tag for the unit test

--- converters_test.go
+++ converters_test.go
@@ -9,16 +9,18 @@ func TestPriorityDescription(t *testing.T) {
 
 	tests := []struct {
 		priority syslog.Priority
-		want string
+		want     string
 	}{
-		{ syslog.LOG_CRIT, "crit"},
-		{ syslog.LOG_WARNING, "warning"},
-		{ syslog.LOG_ERR, "error"},
-		{ syslog.LOG_DEBUG, "debug"},
-		{ syslog.LOG_INFO, "info"},
-		{ syslog.LOG_EMERG, "emerg"},
+		{syslog.LOG_CRIT, "crit"},
+		{syslog.LOG_WARNING, "warning"},
+		{syslog.LOG_ERR, "error"},
+		{syslog.LOG_DEBUG, "debug"},
+		{syslog.LOG_INFO, "info"},
+		{syslog.LOG_EMERG, "emerg"},
 	}
-		for _, tt := range tests {
+	
+	//nolint
+	for _, tt := range tests {
 		t.Run(tt.want, func(t *testing.T) {
 			if got := PriorityDescription(tt.priority); got != tt.want {
 				t.Errorf("PriorityDescription() = %v, want %v", got, tt.want)
