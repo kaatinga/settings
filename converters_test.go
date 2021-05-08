427ed49f460763205fffed99f6e89761526d6b80
Michael Gunkoff <painter@3lines.club>
2021-05-08 16:51:03 +0300

    added unit test to PriorityDescription()

---
+++ converters_test.go
@@ -0,0 +1,28 @@
+package env_loader
+
+import (
+	"log/syslog"
+	"testing"
+)
+
+func TestPriorityDescription(t *testing.T) {
+
+	tests := []struct {
+		priority syslog.Priority
+		want string
+	}{
+		{ syslog.LOG_CRIT, "crit"},
+		{ syslog.LOG_WARNING, "warning"},
+		{ syslog.LOG_ERR, "error"},
+		{ syslog.LOG_DEBUG, "debug"},
+		{ syslog.LOG_INFO, "info"},
+		{ syslog.LOG_EMERG, "emerg"},
+	}
+		for _, tt := range tests {
+		t.Run(tt.want, func(t *testing.T) {
+			if got := PriorityDescription(tt.priority); got != tt.want {
+				t.Errorf("PriorityDescription() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
