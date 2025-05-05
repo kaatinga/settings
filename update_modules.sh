6437897505e41f8ca02a100c4b5e0fb11a0cba3b
Michael Gunkoff <painter@3lines.club>
2025-05-05 21:01:41 +0200

    clean up repo

--- update_modules.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "updating direct dependencies..."
-go list -m -u -f '{{if not (or .Indirect .Main)}}{{.Path}}@latest{{end}}' all | xargs go get
-echo "tidying up..."
-go mod tidy -compat=1.18
