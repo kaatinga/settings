0c3e1c879fe2c213544ece1bef1465353620280c
Michael Gunkoff <painter@3lines.club>
2025-10-15 23:45:32 +0200

    cleanup errors

--- errors.go
+++ errors.go
@@ -1,37 +1,35 @@
 package settings
 
-import cer "github.com/kaatinga/const-errs"
+import "errors"
 
-const (
-	ErrUnsupportedField       cer.Error = "unsupported field type"
-	ErrTheModelHasEmptyStruct cer.Error = "an input struct has no fields"
-	ErrNotAStruct             cer.Error = "the configuration must be a struct"
-	ErrNotAddressable         cer.Error = "the main struct must be pointed out via pointer"
-	ErrNotAddressableField    cer.Error = "the value is not addressable or main struct is not indicated via pointer"
-
-	ErrInternalFailure     cer.Error = "an internal package error"
-	ErrIncorrectFieldValue cer.Error = "variable has been found but has incorrect value"
-	ErrValidationFailed    cer.Error = "field validation failed"
+var (
+	ErrTheModelHasEmptyStruct = errors.New("an input struct has no fields")
+	ErrNotAStruct             = errors.New("the configuration must be a struct")
+	ErrNotAddressable         = errors.New("the main struct must be pointed out via pointer")
+	ErrNotAddressableField    = errors.New("the value is not addressable or main struct is not indicated via pointer")
+	ErrInternalFailure        = errors.New("an internal package error")
 )
 
 type unsupportedFieldError string
 
 func (err unsupportedFieldError) Error() string {
-	return "environment variable " + string(err) + " has been found but the field type is unsupported"
+	return "environment variable '" + string(err) + "' has been found but the field type is unsupported"
 }
 
 func (err unsupportedFieldError) Is(target error) bool {
-	return target == ErrUnsupportedField //nolint:goerr113
+	_, ok := target.(unsupportedFieldError)
+	return ok
 }
 
 type incorrectFieldValueError string
 
 func (err incorrectFieldValueError) Error() string {
-	return "environment variable " + string(err) + " has been found but has incorrect value"
+	return "environment variable '" + string(err) + "' has been found but has incorrect value"
 }
 
 func (err incorrectFieldValueError) Is(target error) bool {
-	return target == ErrIncorrectFieldValue //nolint:goerr113
+	_, ok := target.(incorrectFieldValueError)
+	return ok
 }
 
 type validationFailedError struct {
@@ -45,5 +43,22 @@ func (err *validationFailedError) Error() string {
 }
 
 func (err *validationFailedError) Is(target error) bool {
-	return target == ErrValidationFailed //nolint:goerr113
+	_, ok := target.(*validationFailedError)
+	return ok
+}
+
+func NewUnsupportedFieldError(fieldName string) error {
+	return unsupportedFieldError(fieldName)
+}
+
+func NewIncorrectFieldValueError(fieldName string) error {
+	return incorrectFieldValueError(fieldName)
+}
+
+func NewValidationFailedError(name, fieldType, validationRule string) error {
+	return &validationFailedError{
+		Name:           name,
+		Type:           fieldType,
+		ValidationRule: validationRule,
+	}
 }
